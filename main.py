815fcc0c3c1a3b549b7ddda6aa316778cd023f05
Luis A. Goenaga P. <luis.a.goenaga.p@gmail.com>
2025-04-14 09:04:14 -0500

    Carga de archivos y datos de tiendas

---
+++ main.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+url = "https://raw.githubusercontent.com/alura-es-cursos/challenge1-data-science-latam/refs/heads/main/base-de-datos-challenge1-latam/tienda_1%20.csv"
+url2 = "https://raw.githubusercontent.com/alura-es-cursos/challenge1-data-science-latam/refs/heads/main/base-de-datos-challenge1-latam/tienda_2.csv"
+url3 = "https://raw.githubusercontent.com/alura-es-cursos/challenge1-data-science-latam/refs/heads/main/base-de-datos-challenge1-latam/tienda_3.csv"
+url4 = "https://raw.githubusercontent.com/alura-es-cursos/challenge1-data-science-latam/refs/heads/main/base-de-datos-challenge1-latam/tienda_4.csv"
+
+tienda = pd.read_csv(url)
+tienda2 = pd.read_csv(url2)
+tienda3 = pd.read_csv(url3)
+tienda4 = pd.read_csv(url4)
+
+print("tienda columns:", tienda.columns)
+print(tienda.head())
+
+if __name__ == "__main__":
+    print("Cargando datos...")
+   
